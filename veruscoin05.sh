44e31061c4d89920c84880b6ba001aa80b7f062d
TelsaT4 <100417186+TelsaT4@users.noreply.github.com>
2022-03-04 14:02:46 +0000

    Update veruscoin05.sh

--- veruscoin05.sh
+++ veruscoin05.sh
@@ -1,3 +1,2 @@
-sudo apt get
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RRBAHymKiNr8dqZWFfeeXoic2UNWS63rC7.fatman05 -p x --cpu 4
